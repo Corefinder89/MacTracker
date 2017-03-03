95cf3c6777282758a43b75064206c25a8e7e32f2
SoumyajitBasu1988 <soumyajit.basu@srijan.in>
2017-03-03 12:37:12 +0530

    Adding changes to the shell script for nmap

--- tracker_config/run_nmap.sh
+++ tracker_config/run_nmap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo -s <<EOF
   echo Connected as ROOT
-  nmap -sP -n 192.168.2.* | tee nmap.txt
+  nmap -sP -n 192.168.0.* | tee nmap.txt
   cd ../mac_identifier
   echo Connected to mac_identifier
 EOF
