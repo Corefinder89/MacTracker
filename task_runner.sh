39098c9ed8a5267ad2ffcb4138fa0fcc8e8647e3
SoumyajitBasu1988 <soumyajit.basu@srijan.in>
2017-03-04 17:22:30 +0530

    Tweaked the shell script by removing absolute path

--- task_runner.sh
+++ task_runner.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DIR=mac_identifier
 cd /Users/soumyajit/Documents/git_repos/target_mac/tracker_config
-./Users/soumyajit/Documents/git_repos/target_mac/tracker_config/run_nmap.sh
+./run_nmap.sh
 if [ ! -d "$DIR" ]
 then
   exit
