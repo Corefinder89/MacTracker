6e1307629b831b87b010e9b15ad4cb5d34a630aa
SoumyajitBasu1988 <soumyajit.basu@srijan.in>
2017-03-06 16:57:35 +0530

    Changing text in the nmap shell script

--- tracker_execute/run_nmap.sh
+++ tracker_execute/run_nmap.sh
@@ -3,5 +3,5 @@ sudo -s <<EOF
   DIR=mac_identifier
   echo Connected as ROOT
   /usr/local/bin/nmap -sP -n -oX - 192.168.2.1/24 | tee nmap.xml
-  echo "Tranferring nmap data ......"
+  echo "\n Dumping nmap data onto project directory......"
 EOF
