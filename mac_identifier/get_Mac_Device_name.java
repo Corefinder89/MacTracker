a7203aa2c0bf9f760b98ab4e23f6b1118c755642
SoumyajitBasu1988 <soumyajit.basu@srijan.in>
2017-03-06 16:55:38 +0530

    Addin implementational change, 1. Parsing will be done through xml rather than plain text and 2. Logic is changes to parse xml and generate ip address, mac address and device name

--- mac_identifier/get_Mac_Device_name.java
+++ mac_identifier/get_Mac_Device_name.java
@@ -1,54 +1,55 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.util.Scanner;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
-class get_Mac_Device_name
+public class get_Mac_Device_name
 {
-	public static void get_MacAddress_DeviceName(String line)
-	{
-		String get_param = "";
-		String Device_MacAddress = "";
-		String DeviceName = "";
-		if(line.startsWith("MAC Address:"))
-			get_param = line;
-		System.out.println(get_param);
-//		String[] get_param_array = get_param.split("\\(");
-//		for(String itr:get_param_array)
-//		{
-//			if(itr.contains("MAC Address:"))
-//				Device_MacAddress = itr;
-//			if(itr.contains("\b") && !itr.contains("MAC Address"))
-//				DeviceName = itr;
-//		}
-//		System.out.println(Device_MacAddress);
-//		System.out.println("Device Name: "+DeviceName);
-	}
-
 	public static void main(String[] args) throws Exception
 	{
-		BufferedReader br = null;
-		try
-		{
-			String sCurrentLine;
-			FileInputStream fin_s = new FileInputStream("../tracker_execute/nmap");
-			br = new BufferedReader(new InputStreamReader(fin_s));
-			while((sCurrentLine = br.readLine()) != null)
-			{
-				get_MacAddress_DeviceName(sCurrentLine);
-			}
-		}
-		catch(FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			if(br != null)
-				br.close();
-		}
-	}
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document document = db.parse(new File("../tracker_execute/nmap.xml"));
+        NodeList nodeList = document.getElementsByTagName("address");
+        String get_attr_addr = "";
+
+        /*
+         * Iterating the IP address and the mac address over the network
+         */
+        for(int x=0,size= nodeList.getLength(); x<size; x++)
+        {
+            get_attr_addr = get_attr_addr+nodeList.item(x).getAttributes().getNamedItem("addr").getNodeValue()+",";
+
+        }
+
+        /*
+         * Iterating the nodelist to get the vendor names connected over the network
+         */
+        System.out.println("Vendors connected over the network.......");
+        for(int y=1,size= nodeList.getLength(); y<size; y+=2)
+        {
+        	System.out.println("\t\t"+nodeList.item(y).getAttributes().getNamedItem("vendor").getNodeValue());
+        }
+
+        /*
+         * Applying split function to seggregate the ip address and the mac address from the string
+         */
+        String[] segment = get_attr_addr.split(",");
+        System.out.println("IP Adresses traced in the network .......");
+        for(int i=0;i<segment.length;i+=2)
+        {
+        	System.out.println("\t\t"+segment[i]);
+        }
+
+        /*
+         * Iterate the mac address from the segment array
+         */
+        System.out.println("MAC address for devices connected over the network .......");
+        for(int i=1;i<segment.length;i+=2)
+        {
+        	System.out.println("\t\t"+segment[i]);
+        }
+    }
 }
--- tracker_execute/run_nmap.sh
+++ tracker_execute/run_nmap.sh
@@ -2,6 +2,6 @@
 sudo -s <<EOF
   DIR=mac_identifier
   echo Connected as ROOT
-  /usr/local/bin/nmap -sP -n 192.168.2.1/24 | tee nmap
+  /usr/local/bin/nmap -sP -n -oX - 192.168.2.1/24 | tee nmap.xml
   echo "Tranferring nmap data ......"
 EOF
