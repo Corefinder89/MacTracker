ba9295c242ebfa7a79ee4d2ff620baf0827d2639
SoumyajitBasu1988 <soumyajit.basu62@gmail.com>
2017-03-26 18:36:09 +0530

    Make changes in the nmap task executor

--- tracker_execute/run_nmap.sh
+++ tracker_execute/run_nmap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#sudo -s <<EOF
-#  DIR=mac_identifier
+sudo -s <<EOF
+  DIR=mac_identifier
   echo Connected as ROOT
-  nmap -sP -n -oX - 192.168.2.1/24 | tee nmap.xml
+  nmap -sP -n -oX - 192.168.2.1/24 > nmap.xml
   echo "\n Dumping nmap data onto project directory......"
-#EOF
+EOF
